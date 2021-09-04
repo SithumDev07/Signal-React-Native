912235cbc4f81c0f3f6b2631d480e43bb1f6a880
Sithum Basnayaka <sithum.dashantha@gmail.com>
2021-09-04 09:14:02 +0530

    Saving user to DynamoDB using Cognito

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react';
-import { StyleSheet, View, FlatList } from 'react-native'
+import { StyleSheet, View, FlatList, Text, Pressable } from 'react-native'
 
 import ChatRoomItem from '../components/ChatRoomItem';
 import ChatRooms from '../assets/dummy-data/ChatRooms';
 
-const chatRoom1 = ChatRooms[0];
-const chatRoom2 = ChatRooms[1];
+import { Auth } from 'aws-amplify'
 
 export default function TabOneScreen() {
+
+  const Logout = () => {
+    Auth.signOut();
+  }
+
   return (
     <View style={styles.pageView}>
       <FlatList
@@ -15,6 +19,9 @@ export default function TabOneScreen() {
         renderItem={({ item: chatroom }) => <ChatRoomItem chatRoom={chatroom} />}
         showsVerticalScrollIndicator={false}
       />
+      {/* <Pressable onPress={Logout} style={{ backgroundColor: '#404040', height: 50, borderRadius: 5, alignItems: 'center', justifyContent: 'center', position: 'absolute', bottom: 20, width: '80%', left: '10%' }}>
+        <Text style={{ color: 'white', fontWeight: 'bold' }}>Logout</Text>
+      </Pressable> */}
     </View>
   );
 }
